f6b2481e1fa5496db36cc7f371deb42e6d6768b0
nandoksman <fernandooksman@gmail.com>
2021-07-01 11:44:28 -0300

    updated Settings

--- pokefriends/settings.py
+++ pokefriends/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@tgs3y#p8-mh=rbldce6#_du=qt=l5e0x)46jv%sz4jppcv5-*'
+SECRET_KEY = os.environ.get('POKEFRIEND_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
